696d73e3758d648e2819fd6012d7aa80c7c18707
markdyousef <mark@clai.io>
2017-09-19 17:23:45 +0200

    added entity analysis display

--- app/src/components/Reviews/ReviewAnalysis.js
+++ app/src/components/Reviews/ReviewAnalysis.js
@@ -1,7 +1,45 @@
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import Dialog from 'material-ui/Dialog';
+import nlpData from '../../data/reviews_books_analysis_100';
+import styled from 'styled-components';
+
+const Analysis = styled.section`
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+`;
+
+const TokenContainer = styled.div`
+    margin: 2px;
+`;
+
+const Word = styled.span``;
+
+const EntityContainer = styled.span`
+    display: flex;
+    align-items: center;
+    background-color: rgba(255, 255, 200, 0.5);
+    padding: 5px;
+    border-radius: 3px;
+    border: 1px solid #ddd;
+    span {
+        font-size: 8px;
+        margin-left: 2px;
+    }
+`;
+
+const renderEntity = (text, entity) => 
+    <EntityContainer>
+        {text}
+        <span>
+            {entity}
+        </span>
+    </EntityContainer>
 
 
 export default class extends Component {
@@ -9,17 +47,40 @@ export default class extends Component {
         super(props);
         this.state = {
             open: false,
-            analysis: null
+            show: 'none'
         };
     }
-    componentWillMount() {
-        const { review: {reviewText, summary} } = this.props;
-        console.log(this.props.review);
-        this.analyze(reviewText, summary);
+    renderAnalysis = () => {
+        const { review: {analysis, reviewText} } = this.props;
+        const {show} = this.state;
+        return (
+            <Analysis>
+                {analysis.map((token, index) => {
+                    const {text, entity} = token;
+                    token = <Word>{text}</Word>;
+                    switch(show) {
+                        case 'entity':
+                            if (entity) token=renderEntity(text, entity);
+                            break;
+                        case 'dep':
+                            token = <Word>{text}</Word>;
+                            break;
+                        default:
+                            token = <Word>{text}</Word>;
+                            break;
+                    }
+                    return (
+                        <TokenContainer key={text+index}>
+                            {token}
+                        </TokenContainer>
+                    )
+                })}
+            </Analysis>
+        )
+        
     }
-    analyze = (reviewText, summary) => {
-        this.setState({ analysis: reviewText })
-
+    handleChange = (event, value) => {
+        this.setState({show: value})
     }
     handleOpen = () => this.setState({ open: true })
     handleClose = () => this.setState({ open: false })
@@ -36,7 +97,17 @@ export default class extends Component {
                     open={this.state.open}
                     onRequestClose={this.handleClose}
                 >
-                    {this.state.analysis}
+                <RadioButtonGroup
+                    defaultSelected="none"
+                    name="analysis"
+                    style={{display: 'flex', width: 100}}
+                    onChange={this.handleChange}
+                >
+                    <RadioButton value="none" label="None" />
+                    <RadioButton value="entity" label="Entities"/>
+                    <RadioButton value="dep" label="Dependencies"/>
+                </RadioButtonGroup>
+                    {this.renderAnalysis()}
                 </Dialog>
             </div>
         );
